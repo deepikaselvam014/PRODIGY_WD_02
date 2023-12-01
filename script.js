ca29bf585b75137affa72c2063d6d3b1cab7face
deepikaselvam <104366287+deepikaselvam014@users.noreply.github.com>
2023-12-01 08:04:53 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const btnStart=document.querySelector('.start');
+const btnStop=document.querySelector('.stop');
+const btnReset=document.querySelector('.reset');
+let hrs=min=sec=ms=0,startTimer;
+
+btnStart.addEventListener('click',()=>{
+    btnStart.classList.add("start-active");
+    btnStop.classList.remove("stop-active");
+    startTimer=setInterval(()=>{
+        ms++;
+        if(ms==100){
+            sec++;
+            ms=0;
+        }
+        if(sec==60){
+            min++;
+            sec=0;
+        }
+        if(min==60){
+            hrs++;
+            min=0;
+        }
+        updateDisplay();
+    },10);
+});
+btnStop.addEventListener('click',()=>{
+    clearInterval(startTimer);
+    btnStart.classList.remove("start-active");
+    btnStop.classList.add("stop-active");
+    bt
+});
+btnReset.addEventListener('click',()=>{
+    hrs=min=sec=ms=0;
+    clearInterval(startTimer);
+    updateDisplay();
+
+});
+function updateDisplay(){
+    
+    phrs=hrs<10?'0'+hrs:hrs;
+    pmin=min<10?'0'+min:min;
+    psec=sec<10?'0'+sec:sec;
+    pms=ms<10?'0'+ms:ms;
+    
+    document.querySelector('.hrs').innerText=phrs;
+    document.querySelector('.min').innerHTML=pmin;
+    document.querySelector('.sec').innerText=psec;
+    document.querySelector('.ms').innerText=pms;
+}
